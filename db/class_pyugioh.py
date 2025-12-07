4d0510183a42c527f91e2f1a1561d515f6ad8406
Bisneksual <mab0967@yahoo.com>
2025-12-06 18:48:41 -0600

    Added definitions for spell cards, trap cards, tokens, and various monster types

--- db/class_pyugioh.py
+++ db/class_pyugioh.py
@@ -1,7 +1,7 @@
 import json
 import ast
 from constants import BANLIST_MAP,LINKMARKER_MAP
-#from bitstring import BitArray
+import re
 
 class Card:
 
@@ -35,7 +35,7 @@ class YGOCard(Card):
         self.__images = self.get_data().get('card_images')
 
         _data = self.get_data()
-        self.desc = _data.get('desc').replace("''","")
+        self.desc = _data.get('desc').replace("''","").replace('\u25cf','>')
         self.race = _data.get('race')
         self.hasArchetype = ('archetype' in _data)
 
@@ -65,6 +65,12 @@ class YGOCard(Card):
         self.sets = _sets_dict
 
         self.urlTag = _data.get('ygoprodeck_url').split('/')[-1]
+
+
+        _ban = _data.get('banlist_info')
+        if _ban:
+            _ban_map = {key.split('_')[-1]: BANLIST_MAP.index(_ban[key]) for key in _ban}
+            self.banlist = _ban_map
 
         del _data
 
@@ -105,13 +111,49 @@ class YGOMonster(YGOCard):
             self.desc = _data.get('monster_desc').replace("''","")
 
         self.isTuner = ('Tuner' in _data.get('type'))
+        
+        self.isRitual = ("Ritual" in _data.get('type'))
+        if self.isRitual:
+            _desc = self.desc
+            _findritual = re.findall(r'Ritual Summon(?:ed)? (?:this card )?with .*"(.*?)"(?: Ritual Spell Card)?\.',_desc)
+            self.rSummon = _findritual[0] if len(_findritual)>0 else ""
 
-        _ban = _data.get('banlist_info')
-        if _ban:
-            _ban_map = {key.split('_')[-1]: BANLIST_MAP.index(_ban[key]) for key in _ban}
-            self.banlist = _ban_map
+        self.isGemini = ('Gemini' in _data.get('type'))
+        self.isUnion = ('Union' in _data.get('type'))
+        self.isToon = ('Toon' in _data.get('type'))
 
         del _data
+
+class YGOSpell(YGOCard):
+
+    def __init__(self, data_card):
+        super().__init__(data_card)
+
+        _data = self.get_data()
+
+        self.cardType = 'spell'
+        self.mType= _data.get('race')
+
+        del _data
+
+class YGOTrap(YGOCard):
+
+    def __init__(self, data_card):
+        super().__init__(data_card)
+
+        _data = self.get_data()
+
+        self.cardType = 'trap'
+        self.tType= _data.get('race')
+
+        del _data
+
+class YGOToken(YGOCard):
+    
+    def __init__(self, data_card):
+        super().__init__(data_card)
+
+        self.cardType = "token"
 
 class YGOExtraDeck(YGOMonster):
 
@@ -125,21 +167,21 @@ class YGOExtraDeck(YGOMonster):
         if "Fusion" in _data.get('type'):
             self.xType = 'Fusion'
             
-            desc_split = _data.get('desc').split('\r\n')
+            desc_split = self.desc.split('\r\n')
             self.fCriteria = desc_split[0]
             self.desc = "" if len(desc_split) < 2 else "\r\n".join(desc_split[1:])
         
         elif "Synchro" in _data.get('type'):
             self.xType = "Synchro"
             
-            desc_split = _data.get('desc').split('\r\n')
+            desc_split = self.desc.split('\r\n')
             self.sCriteria = desc_split[0]
             self.desc = "" if len(desc_split) < 2 else "\r\n".join(desc_split[1:])
 
         elif "Link" in _data.get('type'):
             self.xType= "Link"
 
-            desc_split = _data.get('desc').split('\r\n')
+            desc_split = self.desc.split('\r\n')
             self.lCriteria = desc_split[0]
             self.desc = "" if len(desc_split) < 2 else "\r\n".join(desc_split[1:])
             
@@ -147,11 +189,24 @@ class YGOExtraDeck(YGOMonster):
 
             _link = _data.get('linkmarkers')
             self.lMarkers = [x in _link for x in LINKMARKER_MAP]
+        
+        elif 'XYZ' in _data.get('type'):
+            self.xType= "Xyz"
+
+            desc_split = self.desc.split('\n')
+            self.zCriteria = desc_split[0]
+            self.desc = "" if len(desc_split) < 2 else "\n".join(desc_split[1:]).replace('\u25cf','\t>')
+        
+        del _data
 
 if __name__=="__main__":
-    fp = open('/home/bisneksual/Documents/pyugioh/db/sample/link.json','r')
+    fp = open('/home/bisneksual/Documents/pyugioh/db/sample/trap.json','r')
     card = json.load(fp)
     card_data = card['data'][0]
 
-    card = YGOExtraDeck(card_data)
+    card = YGOTrap(card_data)
     print(card.pp())
+
+    #sample_desc = 'You can Ritual Summon this card with "Mitsurugi Ritual". Monsters your opponent controls lose 800 ATK. You can reveal this card in your hand; Special Summon 1 "Mitsurugi" monster from your Deck, then Tribute 1 monster you control. You can only use this effect of "Ame no Habakiri no Mitsurugi" once per Duel. If this card is Tributed: You can add 1 "Mitsurugi" card from your Deck to your hand, except "Ame no Habakiri no Mitsurugi", then you can Special Summon this card. You can only use this effect of "Ame no Habakiri no Mitsurugi" once per turn.'
+    #sample_re = re.findall(r'Ritual Summon(?:ed)? (?:this card )?with .*"(.*?)"(?: Ritual Spell Card)?\.',sample_desc)
+    #print(sample_re)
